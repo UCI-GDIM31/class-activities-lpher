b0d84e93ca3a489ea2fa044f7a3025ea6c8c1bba
lpher <lpher@uci.edu>
2025-11-03 22:56:35 -0800

    W6 In-Class activity update

--- In Class Activities/Assets/W6/Scripts/BatW6.cs
+++ In Class Activities/Assets/W6/Scripts/BatW6.cs
@@ -3,5 +3,32 @@ using UnityEngine;
 // Write the BatW6 class here.
 public class BatW6 : MonoBehaviour
 {
-    
+    [SerializeField] private float _speed = 2.0f;
+    private Transform _playerTransform;
+
+    private void Awake()
+    {
+        enabled = false;
+    }
+
+    public void EnableChase(Transform player)
+    {
+        enabled = true;
+        _playerTransform = player;
+    }
+
+    public void DisableChase()
+    {
+        enabled = false;
+        _playerTransform = null;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            _playerTransform.position,
+            _speed * Time.deltaTime
+        );
+    }
 }
